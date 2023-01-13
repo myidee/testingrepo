aa21060138f4b4289027d82f2efe63c8beabd3dd
Ghaeli <ghaelim@douglascollege.ca>
2023-01-12 17:52:01 -0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,80 @@
+users = [
+    {
+        "name" : "iboya vat",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/67.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "aapo niskanen",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "zilda moreira",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lilou le gall",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/23.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lucy hall",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "sara alves",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/14.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "ramon macrae",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "connor taylor",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/15.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "aymeric morel",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lorenz otto",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "karl williamson",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/29.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "ouassim heering",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "roberto molina",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "",
+        "image" : "",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "",
+        "image" : "",
+        "joined" : "07/15/15"
+    }
+]
+
+
+
